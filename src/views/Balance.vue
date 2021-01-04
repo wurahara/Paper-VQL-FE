9e1146e0155e9d98f4d26286edb6600a8cdd1970
wurahara <wurahara@163.com>
2021-01-04 21:57:17 +0800

    fix html

--- src/views/Balance.vue
+++ src/views/Balance.vue
@@ -1,77 +1,49 @@
 <template>
   <div class="balance">
+
     <h1>Create new balance query</h1>
-
-    <el-row type="flex"><el-col :span="13"><h2>Query balance sum</h2></el-col></el-row>
-
-    <el-row type="flex"><el-col :span="13"><h2>Date range</h2></el-col></el-row>
+    <h2>Query balance sum</h2>
 
     <el-row type="flex" justify="center">
-      <el-col :span="13">
-        <el-date-picker v-model="value1" type="daterange" range-separator="to" start-placeholder="Start date" end-placeholder="End date">
-    </el-date-picker>
-      </el-col>
-    </el-row>
-
-    <el-row type="flex"><el-col :span="14"><h2>Address</h2></el-col></el-row>
-
-    <el-row type="flex" justify="center">
-      <el-col :span="13">
-        <el-input placeholder="Address" v-model="input" clearable></el-input>
-      </el-col>
+      <el-col :span="3"><h3>Date range</h3></el-col>
+      <el-col :span="10"><el-date-picker v-model="value1" type="daterange" range-separator="to" start-placeholder="Start date" end-placeholder="End date"></el-date-picker></el-col>
     </el-row>
 
     <el-row type="flex" justify="center">
-      <el-col :span="13">
-        <el-button type="primary" icon="el-icon-search">Submit</el-button>
-      </el-col>
+      <el-col :span="3"><h3>Address</h3></el-col>
+      <el-col :span="10"><el-input placeholder="Address" v-model="input" clearable></el-input></el-col>
     </el-row>
 
-    <el-row type="flex"><el-col :span="13"><h2>Query balance records</h2></el-col></el-row>
-
-    <el-row type="flex"><el-col :span="13"><h2>Date range</h2></el-col></el-row>
-
     <el-row type="flex" justify="center">
-      <el-col :span="13">
-        <el-date-picker v-model="value1" type="daterange" range-separator="to" start-placeholder="Start date" end-placeholder="End date">
-    </el-date-picker>
-      </el-col>
+      <el-col :span="13"><el-button type="primary" icon="el-icon-search">Submit</el-button></el-col>
     </el-row>
 
-    <el-row type="flex"><el-col :span="14"><h2>Address</h2></el-col></el-row>
+    <h2>Query balance records</h2>
+
+    <el-row type="flex"></el-row>
 
     <el-row type="flex" justify="center">
-      <el-col :span="13">
-        <el-input placeholder="Address" v-model="input" clearable></el-input>
-      </el-col>
+      <el-col :span="3"><h3>Date range</h3></el-col>
+      <el-col :span="10"><el-date-picker v-model="value1" type="daterange" range-separator="to" start-placeholder="Start date" end-placeholder="End date"></el-date-picker></el-col>
     </el-row>
 
-    <el-row type="flex"><el-col :span="14"><h2>Expense range</h2></el-col></el-row>
-
     <el-row type="flex" justify="center">
-      <el-col :span="6">
-        <el-input placeholder="Expense bottom" v-model="input" clearable></el-input>
-      </el-col>
-
-      <el-col :span="1"> to </el-col>
-
-      <el-col :span="6">
-        <el-input placeholder="Expense peak" v-model="input" clearable></el-input>
-      </el-col>
+      <el-col :span="3"><h3>Address</h3></el-col>
+      <el-col :span="10"><el-input placeholder="Address" v-model="input" clearable></el-input></el-col>
     </el-row>
 
-    <el-row type="flex"><el-col :span="14"><h2>Income range</h2></el-col></el-row>
+    <el-row type="flex" justify="center">
+      <el-col :span="3"><h3>Expense range</h3></el-col>
+      <el-col :span="4"><el-input placeholder="Expense bottom" v-model="input" clearable></el-input></el-col>
+      <el-col :span="2"> to </el-col>
+      <el-col :span="4"><el-input placeholder="Expense peak" v-model="input" clearable></el-input></el-col>
+    </el-row>
 
     <el-row type="flex" justify="center">
-      <el-col :span="6">
-        <el-input placeholder="Income bottom" v-model="input" clearable></el-input>
-      </el-col>
-
-      <el-col :span="1"> to </el-col>
-
-      <el-col :span="6">
-        <el-input placeholder="Income peak" v-model="input" clearable></el-input>
-      </el-col>
+      <el-col :span="3"><h3>Income range</h3></el-col>
+      <el-col :span="4"><el-input placeholder="Income bottom" v-model="input" clearable></el-input></el-col>
+      <el-col :span="2"> to </el-col>
+      <el-col :span="4"><el-input placeholder="Income peak" v-model="input" clearable></el-input></el-col>
     </el-row>
 
     <el-row type="flex" justify="center">
@@ -80,7 +52,7 @@
       </el-col>
     </el-row>
 
-    <el-row type="flex"><el-col :span="14"><h2>Query result</h2></el-col></el-row>
+    <h2>Query result</h2>
 
     <el-row type="flex" justify="center">
       <el-col :span="13">
--- src/views/Block.vue
+++ src/views/Block.vue
@@ -1,36 +1,21 @@
 <template>
   <div class="block">
+
     <h1>Create new block query</h1>
 
-    <el-row type="flex"><el-col :span="13"><h2>Hash</h2></el-col></el-row>
-
     <el-row type="flex" justify="center">
-      <el-col :span="13">
-        <el-input placeholder="Hash" v-model="input" clearable></el-input>
-      </el-col>
+      <el-col :span="3"><h3 class="tag">Hash</h3></el-col>
+      <el-col :span="8"><el-input placeholder="Hash" v-model="input" clearable></el-input></el-col>
+      <el-col :span="2"><el-button type="primary" icon="el-icon-search">Submit</el-button></el-col>
     </el-row>
 
     <el-row type="flex" justify="center">
-      <el-col :span="13">
-        <el-button type="primary" icon="el-icon-search">Submit</el-button>
-      </el-col>
+      <el-col :span="3"><h3 class="tag">Block number</h3></el-col>
+      <el-col :span="8"><el-input placeholder="Block number" v-model="input" clearable></el-input></el-col>
+      <el-col :span="2"><el-button type="primary" icon="el-icon-search">Submit</el-button></el-col>
     </el-row>
 
-    <el-row type="flex"><el-col :span="14"><h2>Block number</h2></el-col></el-row>
-
-    <el-row type="flex" justify="center">
-      <el-col :span="13">
-        <el-input placeholder="Block number" v-model="input" clearable></el-input>
-      </el-col>
-    </el-row>
-
-    <el-row type="flex" justify="center">
-      <el-col :span="13">
-        <el-button type="primary" icon="el-icon-search">Submit</el-button>
-      </el-col>
-    </el-row>
-
-    <el-row type="flex"><el-col :span="14"><h2>Query result</h2></el-col></el-row>
+    <h2>Query result</h2>
 
     <el-row type="flex" justify="center">
       <el-col :span="13">
