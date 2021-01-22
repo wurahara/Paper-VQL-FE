5080e1dea02ee7f7f26a6a688903f7f3ee374908
wurahara <wurahara@163.com>
2021-01-22 15:39:12 +0800

    web font test

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,10 @@
 </template>
 
 <style lang="stylus">
+@import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap');
+
 #app
-  font-family Avenir, 'Calibri', 'Arial Nova', 'Lucida Sans Unicode', 'Lucida Grande', sans-serif
+  font-family Avenir, 'Nunito', sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   text-align center
